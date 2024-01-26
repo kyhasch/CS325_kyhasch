4aa300991a976a47dd7853e5a7981e5c5fb3c7c9
Kyle Haschemeyer <kyhasch@siue.edu>
2024-01-26 13:37:12 -0600

    feature 1

--- Helloworld.py
+++ Helloworld.py
@@ -1,7 +1,12 @@
 # File: hello_world.py
 
+def get_user_name():
+    name = input("What's your name? ")
+    return name
+
 def main():
-    print("Hello, World!")
+    user_name = get_user_name()
+    print(f"Hello, {user_name}! Welcome to the world of Python.")
 
 if __name__ == "__main__":
     main()
